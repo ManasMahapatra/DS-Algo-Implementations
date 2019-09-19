26e48050e276383fed56b0dc9122b4716239ddf0
TheSixthPhantom <Chirag301212@gmail.com>
2019-09-19 15:34:58 +0530

    added:MULTIPLE_DELETION_METHODS

--- HashMap.cpp
+++ HashMap.cpp
@@ -75,6 +75,7 @@ class HashMap {
     //DELETION
     //---------------------------------------------------
     void filterMapByValue(int value) {
+        //delete all nodes throughout the hashmap containing the provided value
         node* traverseNode;
         for (int i=0;i<arraySize;i++) {
             traverseNode = primaryArray[i];
@@ -105,6 +106,39 @@ class HashMap {
             }
         }
     }
+    void deleteValueFromKey(int value, int key) {
+        int hashedKey = hashFunction(key);
+        if (primaryArray[hashedKey] == NULL) {
+            cout<<"[-] Mentioned key is already empty.";
+        } else {
+            node* traverseNode = primaryArray[hashedKey];
+            while(traverseNode != NULL) {
+                if (traverseNode->data == value) {
+                    if (traverseNode->prev == NULL){
+                        primaryArray[hashedKey] = traverseNode->next;
+                        if (traverseNode->next != NULL) {
+                            traverseNode->next->prev = NULL;
+                        }
+                        traverseNode = NULL;
+                        break;
+                    }
+                    traverseNode->prev->next = traverseNode->next;
+                    if (traverseNode->next == NULL) {
+                        traverseNode = NULL;
+                        continue;
+                    }
+                    traverseNode->next->prev = traverseNode->prev;
+                    traverseNode = NULL;
+                    continue;
+                }
+                traverseNode = traverseNode->next;
+            }
+        }
+    }
+    void emptyKey(int key) {
+        int hashedKey = hashFunction(key);
+        primaryArray[hashedKey] = NULL;
+    }
     //---------------------------------------------------
     //TRAVERSAL
     //---------------------------------------------------
@@ -125,20 +159,25 @@ class HashMap {
         }
     }
     void findByValue(int value) {
+        int count = 0;
         node* traverseNode;
         for (int i=0;i<arraySize;i++) {
             traverseNode = primaryArray[i];
             if(primaryArray[i] != NULL) {
                 while(traverseNode != NULL) {
                     if (traverseNode->data == value) {
+                        count++;
                         cout<<"[+]Value is found at index: "<<i<<"\n";
-                        break;
                     }
                     traverseNode = traverseNode->next;
                 }
             }
         }
-        cout<<"[-]Value Not Found\n";
+        if (count) {
+            cout<<"[+]Total frequency of occurence: "<<count<<"\n";
+        } else {
+            cout<<"[-]Value Not Found\n";
+        }
     }
     void findByKey(int key) {
         int hashedKey = hashFunction(key);
