1fd4e27ad201256e7e77e028a7d7ac7291c6da5b
TheSixthPhantom <chirag301212@gmail.com>
2019-09-27 17:44:01 +0530

    added:GRAPH_ADD

--- DataStructures/NonLinerarDataStructurres/Graph/GraphListImplementation.cpp
+++ DataStructures/NonLinerarDataStructurres/Graph/GraphListImplementation.cpp
@@ -1,5 +1,28 @@
 #include <stdio.h>
-//List library for using Linked lists. Can create your own but this list requires to hold the node* type
+//List library for using Linked lists. We can use our own self made linked list as well.
 #include <list>
 #include <iostream>
 using namespace std;
+//This variable contains the number of nodes thatthe graph contains. 
+//Can be made dynamic using constructor as well.
+int const totalNodes = 5;
+class GraphList {
+    private:
+    //The implementation will create an array of linkedlists
+    list <int> container[totalNodes];
+    public:
+    void setPath(int source, int destination) {
+        if (source < totalNodes && destination < totalNodes) {
+            container[source].push_back(destination);
+        } else {
+            cout<<"[-] Invalid source or destination";
+        }
+    }
+    void traverseGraph() {
+        
+    }
+};
+int main() {
+    GraphList graph;
+    graph.setPath(0,4);
+}
